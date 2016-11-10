4e483d48b9344837aee308f09d90355b666c8131
Francesco Brozzu <francescobrozzu@gmail.com>
2016-11-10 20:06:44 +0100

    Fixed infinite loop in LAB4.Exercise7.3 by adding control variable (check notes for more info)

--- LAB4/Exercise7.3/main.c
+++ LAB4/Exercise7.3/main.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-//TO BE COMPLETED!
+//francesco capo della cia!!!!
 int main()
 {
-    int value = 1;
-    while (value>0){
+    int value =0;
+    int showvalue = 0;
+    //When int goes into overflow it becomes negative
+    while (value>=0){
+        //Update the control variable showvalue only if the next number is not negative, otherwise we would obtain a negative size of INT
         if ((value+1)>0){
+            value=value+1;
+            showvalue=showvalue+1;
+        }
+        if ((value+1)<0){
             value=value+1;
         }
     }
-    printf("Maximum possible value for int: %d", value);
+    printf("Maximum possible value for int: %d", showvalue);
     return 0;
 }
