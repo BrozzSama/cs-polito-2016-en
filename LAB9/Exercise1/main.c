2cf21739c5d36ba74e60f062fab67f10be721012
Francesco Brozzu <francescobrozzu@gmail.com>
2016-12-19 00:26:33 +0100

    Added comments to LAB9/Exercise1

--- LAB9/Exercise1/main.c
+++ LAB9/Exercise1/main.c
@@ -11,7 +11,7 @@ char swamp[ROWS][COLUMNS];
 int main()
 {
     int pathfound = 0;
-
+    // Get swamp from swamp file!
     FILE* data=fopen("example.swamp", "r");
     for (int i = 0; i<ROWS; i++){
         for (int j = 0; j<COLUMNS; j++){
@@ -21,11 +21,13 @@ int main()
         printf("\n");
     }
     fclose(data);
+    //For every row starting with a rock check if a path exists
     for (int i = 0; i<ROWS && !pathfound; i++){
         if(swamp[i][0]=='*'){
             pathfound=findpath(i);
         }
     }
+    //If the function found a path then print the path array, since it contains a possible path!
     if (pathfound == 1){
         for (int i = 0; i<COLUMNS; i++){
             printf("%d", path[i]);
@@ -34,30 +36,37 @@ int main()
     return 0;
 }
 
+//Search for a path in the swamp starting from the given row: variable position
 int findpath(int position){
     int counter = 0;
     int j = 0;
     path[0]=position;
     while(j<COLUMNS){
+        //If there is a rock on the top, move position to the top
         if (swamp[position][j]==swamp[position+1][j+1]){
             position+=1;
             path[j+1]=position;
             ++counter;
         }
+        //If there is a rock in the row move forward in the row
         else if (swamp[position][j]==swamp[position][j+1]){
             path[j+1]=position;
             ++counter;
         }
+        //If there is a rock on the bottom, move position to the bottom
         else if (swamp[position][j]==swamp[position-1][j+1]){
             position-=1;
             path[j+1]=position;
             ++counter;
         }
+        //Interrupt the loop so we can look for another path starting from another row
         else {
             j = COLUMNS;
         }
         j++;
     }
+    //If counter reached the number of columns-1 (-1 because we are starting from a row already) we found a path
+    //Return POSITIVE result YAY!
     if (counter == COLUMNS - 1){
         return 1;
     }
