e5ce041ce090fe7931ac758a244956a4de0c9676
Francesco Brozzu <francescobrozzu@gmail.com>
2016-12-19 14:07:43 +0100

    Correction to LAB9/Exercise1
    Swamp can be now passed as argument!

--- LAB9/Exercise1/main.c
+++ LAB9/Exercise1/main.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define ROWS 5
-#define COLUMNS 11
+#define ROWS 5 + 2 //(+1 new line and +1 for end of string!)
+#define COLUMNS 11 + 2
 
 int findpath(int position);
 
-int path[COLUMNS];
+int path[COLUMNS-2]; //-2 because of terminator etc...
 char swamp[ROWS][COLUMNS];
 
-int main()
+int main(int argc, char* argv[])
 {
     int pathfound = 0;
     // Get swamp from swamp file!
-    FILE* data=fopen("example.swamp", "r");
+    if (argc != 2){
+        printf("Please specify a swamp file!");
+        exit(EXIT_FAILURE);
+    }
+    FILE* data=fopen(argv[1], "r");
     for (int i = 0; i<ROWS; i++){
-        for (int j = 0; j<COLUMNS; j++){
-            fscanf(data, "%c", &swamp[i][j]);
-            printf("%c", swamp[i][j]);
-        }
-        printf("\n");
+        fgets(swamp[i], COLUMNS, data);
+        printf("%s", swamp[i]);
     }
     fclose(data);
     //For every row starting with a rock check if a path exists
@@ -29,9 +30,10 @@ int main()
     }
     //If the function found a path then print the path array, since it contains a possible path!
     if (pathfound == 1){
-        for (int i = 0; i<COLUMNS; i++){
-            printf("%d", path[i]);
+        for (int i = 0; i<COLUMNS-2; i++){
+            printf("%d ", path[i]);
         }
+        printf("\n");
     }
     return 0;
 }
@@ -41,7 +43,7 @@ int findpath(int position){
     int counter = 0;
     int j = 0;
     path[0]=position;
-    while(j<COLUMNS){
+    while(j<COLUMNS-2){
         //If there is a rock on the top, move position to the top
         if (swamp[position][j]==swamp[position+1][j+1]){
             position+=1;
@@ -61,13 +63,13 @@ int findpath(int position){
         }
         //Interrupt the loop so we can look for another path starting from another row
         else {
-            j = COLUMNS;
+            j = COLUMNS-2;
         }
         j++;
     }
     //If counter reached the number of columns-1 (-1 because we are starting from a row already) we found a path
     //Return POSITIVE result YAY!
-    if (counter == COLUMNS - 1){
+    if (counter == COLUMNS - 3){
         return 1;
     }
     else {
